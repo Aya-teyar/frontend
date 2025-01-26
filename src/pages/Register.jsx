ede8c174d3693e76dd565bcd120dd1ee3c0f740d
Aya <ayateyar6@gmail.com>
2025-01-26 17:02:33 +0100

    l

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import StudentSignup from '../assets/studentsignup.png';
+
 
 const Register = () => {
   const navigate = useNavigate();
